7e328634992e34d1136fec7d6c7d3e19e709e9bf
Kirschn <info@kirschn.de>
2017-03-11 19:04:30 +0100

    initial commit

---
+++ api.php
@@ -0,0 +1,86 @@
+<?php
+include("config.php");
+if (isset($_POST["content"]) && isset($_POST["sessionkey"])) {
+	session_start();
+	if ($_POST["sessionkey"] == $_SESSION["sessionkey"] && $_SESSION["sessionkey"] !== "used") {
+		//$_SESSION["sessionkey"] = "used";
+
+		//Connect databse
+		$link = mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+		if(mysqli_connect_errno()){
+			die("Failed to connect with MySQL");
+		}
+		if ($stmt = mysqli_prepare($link, "INSERT INTO tells (for_uid, content) VALUES (?, ?)")) {
+		    $stmt->bind_param("is", $_POST["foruid"], substr($_POST["content"],0,9000));
+		    $stmt->execute();
+//		    echo var_dump($stmt);
+		    $stmt->close();
+
+
+                   
+		    echo 'Gesendet!';
+
+		    
+		}
+	} else {
+		echo '{"error": true, "reason": "fuck you"}';
+		die();
+	}
+} elseif (isset($_GET["tweetout"]) && isset($_GET["tweetouttoken"]) && isset($_POST["tweettext"])) {
+	session_start();
+	include_once("config.php");
+	if ($_GET["tweetouttoken"] == $_SESSION["tweetouttoken"] && $_SESSION["tweetouttoken"] !== "used") {
+		$screen_name 		= $_SESSION['request_vars']['screen_name'];
+		$twitter_id			= $_SESSION['request_vars']['user_id'];
+		$oauth_token 		= $_SESSION['request_vars']['oauth_token'];
+		$oauth_token_secret = $_SESSION['request_vars']['oauth_token_secret'];
+		//$_SESSION["tweetouttoken"] = "used";
+		//Connect databse
+		$link = mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+		if(mysqli_connect_errno()){
+			die("Failed to connect with MySQL");
+		}
+		if ($stmt = mysqli_prepare($link, "SELECT content FROM tells WHERE id = ? AND for_uid = ?")) {
+			if(isset($_SESSION['status']) && $_SESSION['status'] == 'verified') 
+			{
+				$screen_name 		= $_SESSION['request_vars']['screen_name'];
+				$twitter_id			= $_SESSION['request_vars']['user_id'];
+				$oauth_token 		= $_SESSION['request_vars']['oauth_token'];
+				$oauth_token_secret = $_SESSION['request_vars']['oauth_token_secret'];
+			$twout = $_GET["tweetout"];
+		    $stmt->bind_param("ii", $twout, $twitter_id);
+		    $stmt->execute();
+		    $stmt->bind_result($text);
+		    $stmt->fetch();
+		    $stmt->close();
+		    $im = imagecreatetruecolor(600, 400);
+		    $white = imagecolorallocate($im, 238, 238, 238);
+		    $grey = imagecolorallocate($im, 128, 128, 128);
+		    $black = imagecolorallocate($im, 0, 0, 0);
+		    imagefilledrectangle($im, 0, 0, 599, 399, $white);
+		    $font = '/var/www/tellschn/sintony.ttf';
+		    imagettftext($im, 20, 0, 30, 40, $black, $font, wordwrap($text, 35, "\n"));
+		    imagepng($im, "/var/tellschnimg/".$twout.".png");
+		    if ($stmt = mysqli_prepare($link, "SELECT oauth_token, oauth_secret FROM users WHERE oauth_uid = ?")) {
+		    		$twout = $_GET["tweetout"];
+		    	    $stmt->bind_param("i", $twitter_id);
+		    	    $stmt->execute();
+		    	    $stmt->bind_result($token, $token_secret);
+		    	    $stmt->fetch();
+		    	    $stmt->close();
+		    	include_once('codebird.php');
+		        \Codebird\Codebird::setConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
+		        $cb = \Codebird\Codebird::getInstance();
+		        $cb->setToken($token, $token_secret);
+		        $status = substr($_POST["tweettext"],0,140);
+		        $filename = "/var/tellschnimg/".$twout.".png";
+		        $cb->statuses_updateWithMedia(array('status' => $status, 'media[]' => $filename));
+		        echo "ok";
+		    imagedestroy($im);
+		}
+
+		    }
+		}
+		$link->close();
+	}
+}
---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tellschn</title>
+	<link rel="stylesheet" href="bootstrap.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css?family=Sintony" rel="stylesheet">
+
+	<style type="text/css">
+		body {
+			
+			height: 100%;
+			display: inline-block;
+			font-size: 16px;
+			background: #eee;
+		}
+		.elBackground {
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			top: 0;
+			left: 0;
+			/* background: linear-gradient(rgb(200, 200, 200), white); */
+			z-index: -1;
+
+
+		}
+		.elHeadContainer {
+			width: 100%;
+			text-align: center;
+			position: absolute;
+			animation: fadein 2s;
+			top: 4.5%;
+			animation-fill-mode: forwards; 
+			font-family: 'Sintony', sans-serif;
+			font-weight:100;
+			-webkit-font-smoothing:antialiased;
+			color: #333;
+
+		}
+		.elHeadContainer h1 {
+			font-size: 300%;
+		}
+		.inputElement {
+			margin-left: 25%;
+			margin-right: 25%;
+			width: 50%;
+			text-align: center;
+			position: absolute;
+			top: 39%;
+			animation: fadeinContent 3s;
+			word-wrap: break-word;
+
+ 		}
+ 		.inputElement textarea {
+ 		  resize: vertical; /* user can resize vertically, but width is fixed */
+ 		}
+		@keyframes fadein {
+		    from { opacity: 0; top: 2%;}
+
+		    to   { opacity: 1; top: 17.5% - 50pt;}
+		}
+		@keyframes fadeinContent {
+		    from { opacity: 0;}
+
+		    to   { opacity: 1;}
+		}
+	</style>
+	
+	</head>
+	<bod onload="onload()">
+	<div class="elBackground">&nbsp;</div>
+	<div class="elHeadContainer"><h1>Tellschn</h1><br><br>Der schnelle Weg eine anonyme Meinung zu bekommen</div>
+	<div class="inputElement">
+		<?php
+			include "config.php";
+			session_start();
+			if (!isset($_SESSION["tweetouttoken"]) || $_SESSION["tweetouttoken"] == "used" || $_SESSION["tweetouttoken"] == "") {
+				$_SESSION["tweetouttoken"] = substr(md5(microtime()),rand(0,26),5);
+			}
+			$tweetouttoken = $_SESSION["tweetouttoken"];
+			if(isset($_SESSION['status']) && $_SESSION['status'] == 'verified') 
+			{
+				//Retrive variables
+				$screen_name 		= $_SESSION['request_vars']['screen_name'];
+				$twitter_id			= $_SESSION['request_vars']['user_id'];
+				$oauth_token 		= $_SESSION['request_vars']['oauth_token'];
+				$oauth_token_secret = $_SESSION['request_vars']['oauth_token_secret'];
+				echo 'Hallo <strong>'.$screen_name.'</strong>! Hier ist deine URL:<br><br>';
+				echo '<form>
+  				<div class="input-group">
+    				<input type="text" class="form-control js-copytextarea"
+        				value="http://tell.kirschn.de/'.$screen_name.'" placeholder="" id="copy-input">
+    				<span class="input-group-btn">
+      				<button class="btn btn-default js-textareacopybtn" type="button" id="copy-button"
+          				data-toggle="tooltip" data-placement="button"
+          				title="Copy to Clipboard">
+        				Copy
+  				    </button>
+  				  </span>
+  				</div>
+				</form><br><br>';
+				echo 'Hier sind deine letzten Nachrichten:<br><br><div style="text-align: left">';
+				//Connect databse
+				$link = mysqli_connect($dbServer,$dbUsername,$dbPassword,$dbName);
+				if(mysqli_connect_errno()){
+					die("Failed to connect with MySQL");
+				}
+				if (isset($_GET["page"])) {
+					$page = intval($_GET["page"]);
+				} else {
+					$page = 0;
+				}
+				if ($stmt = mysqli_prepare($link, "SELECT id, content, timestamp FROM tells WHERE for_uid=? ORDER BY id DESC LIMIT ?,10")) {
+				    $stmt->bind_param("ii", $twitter_id, $page);
+				    $stmt->execute();
+				    $stmt->store_result();
+				    $stmt->bind_result($id, $content, $timestamp);
+
+				    while($stmt->fetch())
+				    {
+				        echo "<br><div style='text-align: right'>".DateTime::createFromFormat('Y-m-d H:i:s', $timestamp)->format('d.m.Y H:i:s')." Uhr<br><br>
+				        <button type='button' class='btn btn-primary' onclick='tweetmodal($id)'>Tweet</button></div><br>".htmlspecialchars($content)."<hr>";
+				    }
+
+				    
+				    $stmt->close();
+				}
+				$link->close();
+				//Show welcome message
+				
+				echo '</div><br><br><div style="text-align=right"><a href="/index.php?page='.($page + 10).'"><button type="button" class="btn btn-info" id="nextpage">Nächste Seite</button></a><br><br><br></div>';
+					
+			}else{
+				//Display login button
+				echo '<a href="process.php"><button type="button" class="btn btn-primary">Sign in with Twitter</button></a>';
+			}
+		?>  
+	</div>
+	<script type="text/javascript">
+		function onload() {
+		//	document.getElementById("remaining").addEventListener("input", recalcRemaining);
+		}
+		var copyTextareaBtn = document.querySelector('.js-textareacopybtn');
+
+		copyTextareaBtn.addEventListener('click', function(event) {
+		  var copyTextarea = document.querySelector('.js-copytextarea');
+		  copyTextarea.select();
+
+		  try {
+		    var successful = document.execCommand('copy');
+		    var msg = successful ? 'successful' : 'unsuccessful';
+		    console.log('Copying text command was ' + msg);
+		  } catch (err) {
+		    console.log('Oops, unable to copy');
+		  }
+		});
+		function tweetmodal(id) {
+
+			document.getElementById("tellid").value = id;
+			document.getElementById("tweettext").value = " #Tellschn";
+			recalcRemaining();
+			$("#tweetmodal").modal();
+
+		}
+		function send() {
+			if ((140 - parseInt($("#tweettext").val().length)) >= 0) {
+				$.post("api.php?tweetouttoken=<?php echo $_SESSION["tweetouttoken"]; ?>&tweetout=" + $("#tellid").val(), {
+					"tweettext": $("#tweettext").val().substr(0,140)
+				}, function (res) {
+					console.log(res);
+					console.log("Tweeted!");
+				})
+			}
+		}
+		function recalcRemaining() {
+			document.getElementById("remaining").innerHTML= 140 - parseInt($("#tweettext").val().length);
+		}
+	</script>
+	<div id="tweetmodal" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+
+	    <!-- Modal content-->
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal">&times;</button>
+	        <h4 class="modal-title">Tweet</h4>
+	      </div>
+	      <div class="modal-body">
+	        <p id="modalbody">
+	        	<div class="form-group">
+	        	  <label for="tweettext">Tweet:</label>
+	        	  <input type="text" class="form-control" id="tweettext" oninput="recalcRemaining()"><br>
+	        	  <span id="remaining">140</span>
+	        	  <input type="hidden" value="" id="tellid"><br>
+	        	  Ein Bild mit der Nachricht wird Automatisch an den Tweet angehangen.
+	        	</div>
+	        </p>
+	      </div>
+	      <div class="modal-footer">
+	      <button type="button" class="btn btn-default" data-dismiss="modal" onclick="send()">Tweet</button>
+	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	      </div>
+	    </div>
+
+	  </div>
+	</div>
+	</body>
+</html>
