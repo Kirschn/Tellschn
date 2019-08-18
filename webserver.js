541bfe932a34a8c4cafa208fb0f0f05c3720c418
Kirschn <kirschnkiller@gmail.com>
2019-08-18 22:00:25 +0200

    uff

--- webserver.js
+++ webserver.js
@@ -23,7 +23,7 @@ redisClient.on('error', (err) => {
     console.log('Redis error: ', err);
 });
 app.use(session({
-    secret: conf.cookie_secret,
+    secret: accessconf.cookie_secret,
     name: '_TellschnV2',
     resave: false,
     saveUninitialized: true,
@@ -34,15 +34,15 @@ var kue = require('kue')
     , queue = kue.createQueue();
 var Twitter = require("node-twitter-api");
 var twitter = new Twitter({
-    consumerKey: accessconf.twitter["consumer-key"],
-    consumerSecret: accessconf.twitter["consumer-secret"],
+    consumerKey: accessconf.twitter["consumer_key"],
+    consumerSecret: accessconf.twitter["consumer_secret"],
     callback: accessconf.twitter["redirection_url"]
 });
-conf.mysql.encoding = 'utf8';
-conf.mysql.charset = 'utf8mb4';
+accessconf.mysql.encoding = 'utf8';
+accessconf.mysql.charset = 'utf8mb4';
 var mysql = require('mysql');
 var connection = mysql.createConnection(accessconf.mysql);
-
+var token_secret_index = [];
 
 function nocache(req, res, next) {
     res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
@@ -56,32 +56,32 @@ app.use("/assets", express.static("./webview/assets"));
 app.get("/", function (req, res) {
 
     util.log(port, " /");
-    res.send(info.filledTemplates.index);
+    res.send(JSON.stringify(req.session));
     res.status(200).end();
 });
 
 // twitter API Auth callback
 app.get("/login/twitter_callback", function (req, res) {
     util.log(port, " /twitter_callback hit");
-    if (req.query.oauth_token == null || req.query.oauth_verifier == null || token_secret_index[req.query.oauth_token] == undefined) {
+    if (req.query.oauth_token == null || req.query.oauth_verifier == null || req.session.requestSecret == undefined) {
         res.status(400).end("Query Parameters not set");
         return;
     }
     // verify user login
     util.log("Processing Login Verification for " + req.query.oauth_token);
-    twitter.getAccessToken(req.query.oauth_token, req.query.request_secret, req.query.oauth_verification, function (err, accessToken, accessSecret) {
+    twitter.getAccessToken(req.query.oauth_token, req.session.requestSecret, req.query.oauth_verifier, function (err, accessToken, accessSecret) {
         if (err)
-            return done(err);
+            throw err;
         else
             twitter.verifyCredentials(accessToken, accessSecret, function (err, user) {
                 if (err) {
-                    return done(err);
+                    throw err;
                 } else {
                     user.status = null;
                     user.entities = null;
                     connection.query("SELECT * FROM users WHERE `twitter_id` = ?", user.id_str,
                         function (err, sqlsearchres) {
-                            if (err) { done(err); return; }
+                            if (err) throw err;
                             if (sqlsearchres[0] == undefined) {
                                 util.log("Couldn't find User with ID " + user.id_str + " in user table, inserting new...");
                                 var data = {
@@ -96,7 +96,7 @@ app.get("/login/twitter_callback", function (req, res) {
                                         "shareimgloc": true
                                     }),
                                     "profile_pic_large_uuid": "PENDING",
-                                    "profile_pic_large_uuid": "PENDING"
+                                    "profile_pic_small_uuid": "PENDING"
 
                                 }
 
@@ -105,7 +105,8 @@ app.get("/login/twitter_callback", function (req, res) {
                                         if (err) throw err;
                                         util.log("Inserted data into Table, building Session");
 
-                                        req.session.userpayload = JSON.stringify(data);
+                                        req.session.userpayload = data;
+                                        req.session.requestSecret = undefined;
                                         util.log("Done with User Verify: new user");
 
                                         req.session.save(function(err) {
@@ -117,22 +118,21 @@ app.get("/login/twitter_callback", function (req, res) {
                                 );
                             } else {
                                 util.log("Found User in Database!")
-                                connection.query("UPDATE `users` SET `oauth_token` = ?, `oauth_secret` = ?, `twitter_handle` = ?, `set_name` = ? WHERE `acc-id` = ?",
+                                connection.query("UPDATE `users` SET `oauth_token` = ?, `oauth_secret` = ?, `twitter_handle` = ?, `set_name` = ? WHERE `twitter_id` = ?",
                                     [accessToken, accessSecret, user.screen_name, user.name, user.id_str], function (err) {
                                         if (err) throw err;
-                                        var data = {
-                                            "oauth-token": accessToken,
-                                            "secret": accessSecret,
-                                            "handle": user.screen_name,
-                                            "acc-id": user.id_str,
-                                            "userinfo": JSON.stringify(user)
-                                        };
-                                        util.log("Storing to session...")
-                                        req.session.userpayload = JSON.stringify(data);
-                                        req.session.save(function(err) {
-                                            res.redirect("/" + user.screen_name);
-                                        })
+                                        sqlsearchres[0]["oauth_token"] = accessToken;
+                                        sqlsearchres[0]["oauth_secret"] = accessSecret;
+                                        sqlsearchres[0]["twitter_handle"] = user.screen_name;
+                                        sqlsearchres[0]["set_name"] = user.id_str;
                                         
+                                        util.log("Storing to session...")
+                                        req.session.userpayload = JSON.stringify(sqlsearchres[0]);
+                                        req.session.save(function(err) {
+                                            if (err) throw err;
+                                        })
+                                            res.redirect("/" + user.screen_name);
+                                            res.end();
                                     });
 
 
@@ -153,7 +153,8 @@ app.get("/login/request-token", function (req, res) {
             res.status(500).send("Ein Fehler ist aufgetreten. Kontaktiere bitte @theKirschn oder @nope_js.").end();
             console.log(err);
         } else {
-            token_secret_index[requestToken] = requestSecret;
+            req.session.requestSecret = requestSecret;
+            req.session.save(function(err){});
             res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
         }
     });
@@ -163,3 +164,19 @@ app.get("/login/request-token", function (req, res) {
 app.listen(port, function () {
     util.log(port, 'Webserver online on Port ' + port + '!');
   });
+
+app.get("/api/:endpoint", nocache, function(req, res) {
+    if (req.params.endpoint == "") {
+
+    } else if (req.params.endpoint === "logoff") {
+        req.session.destroy(function() {
+            res.redirect("/");
+        });
+
+
+    }
+});
+
+app.get("/:userpage", nocache, function(req, res) {
+
+});
